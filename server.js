9cc46a5081ffd2b76f777e6ebc273728002343c8
mamelilea <achmalpradiptaaditama@gmail.com>
2024-12-04 05:20:44 +0700

    kelas

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
+app.get('/', (req, res) => {
+    res.json({ message: 'API is running!' });
+  });
 app.use('/api/products', productRoutes);
 app.use('/api/admin', adminRoutes);
 
